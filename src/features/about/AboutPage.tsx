562b32840f0a7e353d8712e3e12b42b2f1a4ea54
Phuong Khanh <Khanh1802@github.com>
2023-09-21 14:05:33 +0700

    Set up router

---
+++ src/features/about/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+const AboutPage = () => {
+    return (
+        <>
+            <Typography variant="h2" >
+                About page
+            </Typography>
+        </>
+    )
+}
+export default AboutPage;
