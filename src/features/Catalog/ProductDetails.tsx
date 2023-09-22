b2a2b185e82f5286ac662c97b33b86385cc029bf
Phuong Khanh <Khanh1802@github.com>
2023-09-22 09:06:03 +0700

    Adding the product detail page content

--- src/features/Catalog/ProductDetails.tsx
+++ src/features/Catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -27,9 +27,46 @@ const ProductDetails = () => {
     }
     return (
         <>
-            <Typography variant="h2" >
-                {product.name}
-            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }}></img>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h3">
+                        {product.name}
+                    </Typography>
+                    <Divider />
+                    <Typography variant="h4" color='secondary' mt={2}>
+                        {(product.price / 100).toFixed(2)}
+                    </Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>{product.name}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Description</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Type</TableCell>
+                                    <TableCell>{product.type}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Brand</TableCell>
+                                    <TableCell>{product.brand}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Quantity in stock</TableCell>
+                                    <TableCell>{product.quantityInStock}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
         </>
     )
 }
